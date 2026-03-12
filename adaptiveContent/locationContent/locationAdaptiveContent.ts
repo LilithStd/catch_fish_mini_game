16491440ef75efa7cefe9a10e976efed72376204
Dmitrii Konevnin <dmitrii.konevnin@gmail.com>
2026-03-12 13:51:50 +0200

    created location adaptive content

---
+++ adaptiveContent/locationContent/locationAdaptiveContent.ts
@@ -0,0 +1,61 @@
+export const locationAdaptiveContent = {
+    en:{
+        mainTitle: "Choose location:",
+        locationsList:[
+            {
+                id: 'lake',
+                name: "Lake",
+                listAvaliblePlaces: [
+                    {
+                        id: 'lakePlace1',
+                        name: "Place 1"
+                    },
+                    {
+                        id: 'lakePlace2',
+                        name: "Place 2"
+                    },
+                ]
+            }
+        ]
+
+    },
+    lv:{
+        mainTitle: "Izvēlieties vietu:",
+        locationsList:[
+            {
+                id: 'lake',
+                name: "Ezers",
+                listAvaliblePlaces: [
+                    {
+                        id: 'lakePlace1',
+                        name: "Vieta 1"
+                    },
+                    {
+                        id: 'lakePlace2',
+                        name: "Vieta 2"
+                    },
+                ]
+            }
+        ]
+    },
+    ru:{
+        mainTitle: "Выберите локацию:",
+        locationsList:[
+            {
+                id: 'lake',
+                name: "Озеро",
+                listAvaliblePlaces: [
+                    {
+                        id: 'lakePlace1',
+                        name: "Место 1"
+                    },
+                    {
+                        id: 'lakePlace2',
+                        name: "Место 2"
+                    },
+                ]
+            }
+        ]
+    },
+
+}
