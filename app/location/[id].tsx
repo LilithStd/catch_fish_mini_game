a70abcf655ccfa28424f8917e6d5b70d9bb38c38
Dmitrii <dmitrii.konevnin@gmail.com>
2026-03-14 20:02:17 +0200

    created methdo to get location data in component location

--- app/location/[id].tsx
+++ app/location/[id].tsx
@@ -1,3 +1,5 @@
+import { useGlobalStore } from "@/store/global/globalStore";
+import { useLocationStore } from "@/store/location/locationStore";
 import { useLocalSearchParams } from "expo-router";
 import { Text } from "react-native";
 import { SafeAreaView } from "react-native-safe-area-context";
@@ -8,9 +10,14 @@ import { SafeAreaView } from "react-native-safe-area-context";
 
 export default function SelectedLocation() {
     const { id } = useLocalSearchParams();
+    const locationId = Array.isArray(id) ? id[0] : id;
+    const currentLanguage = useGlobalStore((state) => state.currentLanguage)
+    const getCurrentLocationData = useLocationStore((state) => state.getCurrentLocationData)
+    const locationData = getCurrentLocationData(locationId, currentLanguage)
+    console.log("locationData", locationData)
     return (
         <SafeAreaView>
-            <Text>Selected Location: {id}</Text>
+            <Text>Selected Location: {locationData.name}</Text>
         </SafeAreaView>
     )
 }
--- store/location/locationStore.ts
+++ store/location/locationStore.ts
@@ -1,27 +1,31 @@
 
+import { locationAdaptiveContent } from '@/adaptiveContent/locationContent/locationAdaptiveContent'
+import { LanguageEnum } from '@/constants/global/enum'
+import { LocationAdaptiveContentType, LocationsItemsType } from '@/constants/types/locationDataTypes'
 import AsyncStorage from '@react-native-async-storage/async-storage'
 import { create } from 'zustand'
 import { createJSONStorage, persist } from 'zustand/middleware'
 
 interface LocationStoreInterface {
   currentLocation: string
-  locationData: ''
+  locationData: LocationAdaptiveContentType
   setCurrentLocation: (location: string) => void
-  getCurrentLocationData: (id: string) => string
+  getCurrentLocationData: (id: string, locale: LanguageEnum) => LocationsItemsType
 }
-
+  
 export const useLocationStore = create<LocationStoreInterface>()(
   persist(
     (set,get) => ({
       currentLocation: '',
-      locationData: '',
+      locationData: locationAdaptiveContent,
       setCurrentLocation: (location) => { 
         if (location !== get().currentLocation) {
           set({ currentLocation: location })
         }
       },
-      getCurrentLocationData: (id) => {
-        return '';
+      getCurrentLocationData: (id, locale) => {
+        const locationList = get().locationData[locale].locationsList
+        return locationList.find(location => location.id === id) as LocationsItemsType
       },
     }),
     {
