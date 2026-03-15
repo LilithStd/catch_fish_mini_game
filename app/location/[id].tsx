e0193a0a7b851ab8ee0949eb5db57eb3d0149cd3
Dmitrii <dmitrii.konevnin@gmail.com>
2026-03-15 20:54:01 +0200

    added position mapPlace pin for test

--- app/location/[id].tsx
+++ app/location/[id].tsx
@@ -19,9 +19,9 @@ export default function SelectedLocation() {
         <SafeAreaView>
             <ImageBackground source={locationData.imageLocation} style={SelectedLocationStyles.ImageBackground} resizeMode="cover">
                 <MapPinSolidIcon width={50} height={50} fill={'white'} style={[SelectedLocationStyles.mapPinIcon, { left: 50, top: 380 }]} />
-                <MapPinSolidIcon width={50} height={50} fill={'white'} style={[SelectedLocationStyles.mapPinIcon, { left: 50, top: 140 }]} />
-                <MapPinSolidIcon width={50} height={50} fill={'white'} style={[SelectedLocationStyles.mapPinIcon, { left: 50, top: 380 }]} />
-                <MapPinSolidIcon width={50} height={50} fill={'white'} style={[SelectedLocationStyles.mapPinIcon, { left: 50, top: 380 }]} />
+                <MapPinSolidIcon width={50} height={50} fill={'white'} style={[SelectedLocationStyles.mapPinIcon, { left: 50, top: 200 }]} />
+                <MapPinSolidIcon width={50} height={50} fill={'white'} style={[SelectedLocationStyles.mapPinIcon, { right: 10, bottom: 160 }]} />
+                <MapPinSolidIcon width={50} height={50} fill={'white'} style={[SelectedLocationStyles.mapPinIcon, { right: -10, top: 280 }]} />
                 <Text style={{ color: "white", fontSize: 24, fontWeight: "bold", textShadowColor: "rgba(0, 0, 0, 0.75)", textShadowOffset: { width: -1, height: 1 }, textShadowRadius: 10 }}>{locationData.name}</Text>
             </ImageBackground>
             <Text>Selected Location: {locationData.name}</Text>
@@ -34,6 +34,7 @@ export const SelectedLocationStyles = StyleSheet.create({
     ImageBackground: {
         width: "100%",
         height: "100%",
+        position: "relative",
         justifyContent: "center",
         alignItems: "center"
     },
