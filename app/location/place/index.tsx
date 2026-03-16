b5f0258ab29366fc17a1f562f97afffcbd1fd64c
Dmitrii <dmitrii.konevnin@gmail.com>
2026-03-16 20:15:31 +0200

    created place route

---
+++ app/location/place/index.tsx
@@ -0,0 +1,16 @@
+import { useGlobalStore } from "@/store/global/globalStore";
+import { useLocationStore } from "@/store/location/locationStore";
+import { useLocalSearchParams } from "expo-router";
+import { View } from "react-native";
+
+
+export default function Place() {
+    const { id } = useLocalSearchParams();
+    const locationId = Array.isArray(id) ? id[0] : id;
+    // stores
+    const currentLanguage = useGlobalStore((state) => state.currentLanguage)
+    const getCurrentLocationData = useLocationStore((state) => state.getCurrentLocationData)
+    return (
+        <View>index</View>
+    )
+}
