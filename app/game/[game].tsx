5287d7d457fa8e1046f99294beeaca080490f6be
Dmitrii <dmitrii.konevnin@gmail.com>
2026-03-27 21:48:12 +0200

    added start game button for game screen

--- app/game/[game].tsx
+++ app/game/[game].tsx
@@ -2,23 +2,35 @@
 import { useGlobalStore } from "@/store/global/globalStore";
 import { useLocationStore } from "@/store/location/locationStore";
 import { useLocalSearchParams } from "expo-router";
-import { ImageBackground, StyleSheet, Text, View } from "react-native";
+import { ImageBackground, StyleSheet, Text, TouchableOpacity, View } from "react-native";
 import { SafeAreaView } from "react-native-safe-area-context";
-
+const ImageStartGame = require("@/assets/images/ui/fishHook.png")
 
 export default function Game() {
     const { game } = useLocalSearchParams();
     const placeId = Array.isArray(game) ? game[0] : game;
+    // stores
     const currentLanguage = useGlobalStore((state) => state.currentLanguage)
     const getLocationPlaceData = useLocationStore((state) => state.getCurrentPlaceData)
-    console.log("game screen id: ", getLocationPlaceData(placeId, currentLanguage))
+    // state
+    // components
+    const buttonStartGame = () => {
+        return (
+            <TouchableOpacity style={GameStyles.buttonStartGame}>
+                <Text style={GameStyles.titleText}>Catch!</Text>
+                <ImageBackground source={ImageStartGame} style={GameStyles.buttonStartGameImage} />
+            </TouchableOpacity>
+        )
+    }
+    // 
+
     return (
         <SafeAreaView style={GameStyles.mainContainer}>
             <ImageBackground source={getLocationPlaceData(placeId, currentLanguage).images.game} resizeMode="cover" style={GameStyles.imageBackground}>
                 <View style={GameStyles.titleContainer}>
                     <Text style={GameStyles.titleText}>{getLocationPlaceData(placeId, currentLanguage).name}</Text>
                 </View>
-
+                {buttonStartGame()}
             </ImageBackground>
         </SafeAreaView>
     )
@@ -30,8 +42,6 @@ const GameStyles = StyleSheet.create({
     },
     imageBackground: {
         flex: 1,
-        justifyContent: 'center',
-        alignItems: 'center'
     },
     descriptionContainer: {
         backgroundColor: 'rgba(255, 255, 255, 0.8)',
@@ -52,6 +62,21 @@ const GameStyles = StyleSheet.create({
         fontWeight: 'bold',
         marginBottom: 10
     },
+    buttonStartGame: {
+        backgroundColor: 'rgba(255, 255, 255, 0.8)',
+        borderRadius: 100,
+        position: 'absolute',
+        bottom: 200,
+        left: 200,
+        width: 150,
+        height: 150,
+        justifyContent: 'center',
+        alignItems: 'center'
+    },
+    buttonStartGameImage: {
+        width: 50,
+        height: 50
+    },
     fishListContainer: {
         backgroundColor: 'rgba(255, 255, 255, 0.8)',
         padding: 20,
@@ -59,8 +84,5 @@ const GameStyles = StyleSheet.create({
         width: '80%',
         alignItems: 'center'
     },
-    image: {
-        resizeMode: 'contain'
-    }
 })
 
