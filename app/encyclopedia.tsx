6bb96f93564eebf16625d7b773c3154a104299d7
Dmitrii <dmitrii.konevnin@gmail.com>
2026-06-20 21:43:34 +0300

    updated fish encyclopedia component

--- app/encyclopedia.tsx
+++ app/encyclopedia.tsx
@@ -1,13 +1,22 @@
+import { encyclopediaAdaptiveContent } from "@/adaptiveContent/encyclopediaAdaptiveContent/encyclopediaAdaptiveContent";
 import { useGlobalStore } from "@/store/global/globalStore";
-import { Text } from "react-native";
+import { StyleSheet, Text } from "react-native";
 import { SafeAreaView } from "react-native-safe-area-context";
 
 
 export default function Encyclopedia() {
     const currentLanguage = useGlobalStore((state) => state.currentLanguage)
     return (
-        <SafeAreaView>
-            <Text>Fish Encyclopedia</Text>
+        <SafeAreaView style={EncyclopediaStyles.mainContainer}>
+            <Text>{encyclopediaAdaptiveContent[currentLanguage].encyclopediaTitle}</Text>
         </SafeAreaView>
     )
 }
+
+export const EncyclopediaStyles = StyleSheet.create({
+    mainContainer: {
+        flex: 1,
+        alignItems: "center",
+        justifyContent: "center",
+    },
+})
