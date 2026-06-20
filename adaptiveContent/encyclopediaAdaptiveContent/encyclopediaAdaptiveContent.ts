30712c144d08d32ff2dc3eac4c9d673d39a6cef4
Dmitrii <dmitrii.konevnin@gmail.com>
2026-06-20 21:27:21 +0300

    updated fish encyclopedia content

---
+++ adaptiveContent/encyclopediaAdaptiveContent/encyclopediaAdaptiveContent.ts
@@ -0,0 +1,23 @@
+export const encyclopediaAdaptiveContent = {
+    en: {
+        encyclopediaTitle: "Fish Encyclopedia",
+        encyclopediaDescription: "The fish encyclopedia is a comprehensive collection of information and knowledge on various fish species, providing detailed explanations, definitions, and insights.",
+        encyclopediaSearchPlaceholder: "Search...",
+        encyclopediaNoResultsFound: "No results found.",
+        encyclopediaBackButton: "Back",
+    },
+    lv: {
+        encyclopediaTitle: "Zivju enciklopēdija",
+        encyclopediaDescription: "Zivju enciklopēdija ir visaptveroša informācijas un zināšanu kolekcija par dažādiem zivju veidiem, sniedzot detalizētus skaidrojumus, definīcijas un ieskatus.",
+        encyclopediaSearchPlaceholder: "Meklēt...",
+        encyclopediaNoResultsFound: "Rezultāti nav atrasti.",
+        encyclopediaBackButton: "Atpakaļ",
+    },
+    ru: {
+        encyclopediaTitle: "Рыбная энциклопедия",
+        encyclopediaDescription: "Рыбная энциклопедия - это обширная коллекция информации и знаний о различных видах рыб, предоставляющая подробные объяснения, определения и инсайты.",
+        encyclopediaSearchPlaceholder: "Поиск...",
+        encyclopediaNoResultsFound: "Результаты не найдены.",
+        encyclopediaBackButton: "Назад",
+    }
+}
