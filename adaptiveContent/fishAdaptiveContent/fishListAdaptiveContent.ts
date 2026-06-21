712be272904a31520a7904b50998d645c40711bf
Dmitrii <dmitrii.konevnin@gmail.com>
2026-06-21 19:35:16 +0300

    replace fish id for one object

--- adaptiveContent/fishAdaptiveContent/fishListAdaptiveContent.ts
+++ adaptiveContent/fishAdaptiveContent/fishListAdaptiveContent.ts
@@ -67,7 +67,7 @@ const fishImages = {
 export const fishListAdaptiveContent = {
     en:{
         pike:{ 
-            id: 'pike',
+            id: fishIds.pike,
             name: "Pike",
             type: typesOfFish.en.predator,
             description: "The pike is a carnivorous fish that is known for its elongated body and sharp teeth. It is a popular game fish and can be found in freshwater habitats across the Northern Hemisphere.",
@@ -75,7 +75,7 @@ export const fishListAdaptiveContent = {
             preview: fishImages.pike.preview
         },
         perch: { 
-            id: 'perch',
+            id: fishIds.perch,
             name: "Perch",
             type: typesOfFish.en.predator,
             description: "The perch is a freshwater fish known for its distinctive stripes and spiny fins. It is a popular target for anglers due to its aggressive nature and tasty flesh.",
@@ -84,7 +84,7 @@ export const fishListAdaptiveContent = {
 
         },
         pikeperch: { 
-            id: 'pikeperch',
+            id: fishIds.pikeperch,
             name: "Pikeperch",
             type: typesOfFish.en.predator,
             description: "The pikeperch is a predatory fish found in freshwater lakes and rivers. It is known for its elongated body and sharp teeth, making it a formidable hunter.",
@@ -93,7 +93,7 @@ export const fishListAdaptiveContent = {
 
         },
         salmon: { 
-            id: 'salmon',
+            id: fishIds.salmon,
             name: "Salmon",
             type: typesOfFish.en.predator,
             description: "The salmon is a migratory fish known for its remarkable journey from freshwater to the ocean and back. It is highly valued for its taste and nutritional benefits.",
@@ -101,7 +101,7 @@ export const fishListAdaptiveContent = {
             preview: fishImages.salmon.preview
         },
         roach: { 
-            id: 'roach',
+            id: fishIds.roach,
             name: "Roach",
             type: typesOfFish.en.omnivorous,
             description: "The roach is a small freshwater fish commonly found in rivers and lakes. It is known for its adaptability and is a popular target for anglers.",
@@ -109,7 +109,7 @@ export const fishListAdaptiveContent = {
             preview: fishImages.roach.preview
         },
         carp: { 
-            id: 'carp',
+            id: fishIds.carp,
             name: "Carp",
             type: typesOfFish.en.nonPredator,
             description: "The carp is a large freshwater fish known for its strength and resilience. It is a popular target for anglers and is often found in lakes and slow-moving rivers.",
@@ -117,7 +117,7 @@ export const fishListAdaptiveContent = {
             preview: fishImages.carp.preview
         },
         trout: { 
-            id: 'trout',
+            id: fishIds.trout,
             name: "Trout",
             type: typesOfFish.en.omnivorous,
             description: "The trout is a freshwater fish known for its colorful appearance and delicate flavor. It is a popular choice for anglers and is often found in cold, clear streams and lakes.",
@@ -125,7 +125,7 @@ export const fishListAdaptiveContent = {
             preview: fishImages.trout.preview
         },
         bream: { 
-            id: 'bream',
+            id: fishIds.bream,
             name: "Bream",
             type: typesOfFish.en.nonPredator,
             description: "The bream is a freshwater fish known for its flat body and silvery scales. It is commonly found in lakes and slow-moving rivers.",
@@ -135,7 +135,7 @@ export const fishListAdaptiveContent = {
     },
     lv:{
         pike:{
-            id: 'pike',
+            id: fishIds.pike,
             name: "Līdaka",
             type: typesOfFish.lv.predator,
             description: "Līdaka ir plēsīga saldūdens zivs, kas pazīstama ar savu garo ķermeni un asiem zobiem, padarot to par bīstamu mednieku.",
@@ -143,7 +143,7 @@ export const fishListAdaptiveContent = {
             preview: fishImages.pike.preview
         },
         perch: {
-            id: 'perch',
+            id: fishIds.perch,
             name: "Asaris",
             type: typesOfFish.lv.predator,
             description: "Asaris ir saldūdens zivs, kas pazīstama ar savām raksturīgajām svītrām un spīļveida spuru. Tas ir populārs makšķernieku mērķis tā agresīvās dabas un garšīgās gaļas dēļ.",
@@ -151,7 +151,7 @@ export const fishListAdaptiveContent = {
             preview: fishImages.perch.preview
         },
         pikeperch: {
-            id: 'pikeperch',
+            id: fishIds.pikeperch,
             name: "Salatis",
             type: typesOfFish.lv.predator,
             description: "Salatis ir plēsīga saldūdens zivs, kas pazīstama ar savu garo ķermeni un asiem zobiem. Tas ir populārs makšķernieku mērķis tā spēka un ātruma dēļ.",
@@ -159,7 +159,7 @@ export const fishListAdaptiveContent = {
             preview: fishImages.pikeperch.preview
         },
         salmon: {
-            id: 'salmon',
+            id: fishIds.salmon,
             name: "Lasis",
             type: typesOfFish.lv.predator,
             description: "Lasis ir plēsīga saldūdens zivs, kas pazīstama ar savu garo ķermeni un asiem zobiem. Tas ir populārs makšķernieku mērķis tā spēka un ātruma dēļ.",
@@ -167,7 +167,7 @@ export const fishListAdaptiveContent = {
             preview: fishImages.salmon.preview
         },
         roach: {
-            id: 'roach',
+            id: fishIds.roach,
             name: "Reņģe",
             type: typesOfFish.lv.omnivorous,
             description: "Reņģe ir neliela saldūdens zivs, kas bieži sastopama upēs un ezeros. Tā ir pazīstama ar savu pielāgošanās spēju un ir populārs makšķernieku mērķis.",
@@ -175,7 +175,7 @@ export const fishListAdaptiveContent = {
             preview: fishImages.roach.preview
         },
         carp: {
-            id: 'carp',
+            id: fishIds.carp,
             name: "Karpa",
             type: typesOfFish.lv.nonPredator,
             description: "Karpa ir liela saldūdens zivs, kas pazīstama ar savu spēku un izturību. Tā ir populārs makšķernieku mērķis un bieži sastopama ezeros un lēni tekošās upēs.",
@@ -183,7 +183,7 @@ export const fishListAdaptiveContent = {
             preview: fishImages.carp.preview
         },
         trout: {
-            id: 'trout',
+            id: fishIds.trout,
             name: "Forele",
             type: typesOfFish.lv.omnivorous,
             description: "Forele ir saldūdens zivs, kas pazīstama ar savu krāsaino izskatu un smalko garšu. Tā ir populāra makšķernieku izvēle un bieži sastopama aukstos, skaidros strautos un ezeros.",
@@ -191,7 +191,7 @@ export const fishListAdaptiveContent = {
             preview: fishImages.trout.preview
         },
         bream: {
-            id: 'bream',
+            id: fishIds.bream,
             name: "Plaudis",
             type: typesOfFish.lv.nonPredator,
             description: "Plaudis ir saldūdens zivs, kas pazīstama ar savu plāno ķermeni un sudrabaino zvīņu krāsu. Tā bieži sastopama ezeros un lēni tekošās upēs.",
@@ -201,7 +201,7 @@ export const fishListAdaptiveContent = {
     },
     ru:{
         pike:{
-            id: 'pike',
+            id: fishIds.pike,
             name: "Щука",
             type: typesOfFish.ru.predator,
             description: "Щука - это хищная пресноводная рыба, известная своим длинным телом и острыми зубами, что делает ее опасным охотником.",
@@ -209,7 +209,7 @@ export const fishListAdaptiveContent = {
             preview: fishImages.pike.preview
         },
         perch: {
-            id: 'perch',
+            id: fishIds.perch,
             name: "Окунь",
             type: typesOfFish.ru.predator,
             description: "Окунь - это хищная пресноводная рыба, известная своим агрессивным поведением и острыми зубами, что делает его опасным охотником.",
@@ -217,7 +217,7 @@ export const fishListAdaptiveContent = {
             preview: fishImages.perch.preview
         },
         pikeperch: {
-            id: 'pikeperch',
+            id: fishIds.pikeperch,
             name: "Судак",
             type: typesOfFish.ru.predator,
             description: "Судак - это хищная пресноводная рыба, известная своим длинным телом и острыми зубами, что делает его опасным охотником.",
@@ -225,7 +225,7 @@ export const fishListAdaptiveContent = {
             preview: fishImages.pikeperch.preview
         },
         salmon: {
-            id: 'salmon',
+            id: fishIds.salmon,
             name: "Лосось",
             type: typesOfFish.ru.predator,
             description: "Лосось - это хищная пресноводная рыба, известная своим длинным телом и острыми зубами, что делает его опасным охотником.",
@@ -233,7 +233,7 @@ export const fishListAdaptiveContent = {
             preview: fishImages.salmon.preview
         },
         roach: {
-            id: 'roach',
+            id: fishIds.roach,
             name: "Плотва",
             type: typesOfFish.ru.omnivorous,
             description: "Плотва - это всеядная пресноводная рыба, известная своей способностью адаптироваться к различным условиям среды и быть популярной целью для рыболовов.",
@@ -241,7 +241,7 @@ export const fishListAdaptiveContent = {
             preview: fishImages.roach.preview
         },
         carp: {
-            id: 'carp',
+            id: fishIds.carp,
             name: "Карп",
             type: typesOfFish.ru.nonPredator,
             description: "Карп - это крупная пресноводная рыба, известная своей силой и выносливостью. Она является популярной целью для рыболовов и часто встречается в озерах и медленно текущих реках.",
@@ -249,7 +249,7 @@ export const fishListAdaptiveContent = {
             preview: fishImages.carp.preview
         },
         trout: {
-            id: 'trout',
+            id: fishIds.trout,
             name: "Форель",
             type: typesOfFish.ru.omnivorous,
             description: "Форель - это пресноводная рыба, известная своим вкусным мясом и красивой окраской. Она часто встречается в холодных, чистых реках и озерах.",
@@ -258,7 +258,7 @@ export const fishListAdaptiveContent = {
 
         },
         bream: {
-            id: 'bream',
+            id: fishIds.bream,
             name: "Лещ",
             type: typesOfFish.ru.nonPredator,
             description: "Лещ - это пресноводная рыба, известная своим плоским телом и серебристой окраской. Он часто встречается в озерах и медленно текущих реках.",
