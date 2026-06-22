0291d5f8b603190fdd1bb9cecf02cc0e612c92ae
Dmitrii <dmitrii.konevnin@gmail.com>
2026-06-22 20:14:11 +0300

    updated encyclopedia component (added fish content and styles)

--- app/encyclopedia.tsx
+++ app/encyclopedia.tsx
@@ -1,17 +1,34 @@
 import { encyclopediaAdaptiveContent } from "@/adaptiveContent/encyclopediaAdaptiveContent/encyclopediaAdaptiveContent";
+import { useEncyclopediaStore } from "@/store/encyclopedia/encyclopediaStore";
 import { useGlobalStore } from "@/store/global/globalStore";
-import { StyleSheet, Text, View } from "react-native";
+import { FlatList, StyleSheet, Text, View } from "react-native";
 import { SafeAreaView } from "react-native-safe-area-context";
 
 
 export default function Encyclopedia() {
     const currentLanguage = useGlobalStore((state) => state.currentLanguage)
+    const encyclopediaData = useEncyclopediaStore((state) => state.encyclopediaData)
     return (
         <SafeAreaView style={EncyclopediaStyles.mainContainer}>
             <View style={EncyclopediaStyles.contentContainer}>
                 <Text>{encyclopediaAdaptiveContent[currentLanguage].encyclopediaTitle}</Text>
                 <Text>{encyclopediaAdaptiveContent[currentLanguage].encyclopediaDescription}</Text>
+                <View>
+                <Text>Fish List:</Text>
+                <FlatList
+                    data={encyclopediaData.fish[currentLanguage]}
+                    keyExtractor={(item) => item.id}
+                    style={EncyclopediaStyles.fishItemContainer}
+                    renderItem={({ item }) => (
+                        <View style={EncyclopediaStyles.fishItemContainer}>
+                            <Text>Name: {item.name}</Text>
+                            <Text>Type: {item.type}</Text>
+                        </View>
+                    )}
+                />
             </View>
+            </View>
+            
         </SafeAreaView>
     )
 }
@@ -28,5 +45,10 @@ export const EncyclopediaStyles = StyleSheet.create({
         width: "100%",
         padding: 20,
         backgroundColor: "lime",
-    }
+    },
+    fishItemContainer: {
+        padding: 10,
+            borderBottomWidth: 1,
+            borderBottomColor: "#ccc",
+    },
 })
--- store/encyclopedia/encyclopediaStore.ts
+++ store/encyclopedia/encyclopediaStore.ts
@@ -1,3 +1,4 @@
+import { fishContent } from '@/adaptiveContent/fishAdaptiveContent/fishListAdaptiveContent'
 import { LanguageEnum } from '@/constants/global/enum'
 import { encyclopediaDataTypes } from '@/constants/types/encyclopediaDataTypes'
 import AsyncStorage from '@react-native-async-storage/async-storage'
@@ -5,14 +6,16 @@ import { create } from 'zustand'
 import { createJSONStorage, persist } from 'zustand/middleware'
 
 interface EncyclopediaStoreInterface {
-  encyclopediaData:encyclopediaDataTypes[],
+  encyclopediaData:encyclopediaDataTypes,
   getResponseInformation: (language: LanguageEnum) => void
 }
 
 export const useEncyclopediaStore = create<EncyclopediaStoreInterface>()(
   persist(
     (set,get) => ({
-        encyclopediaData: [],  
+        encyclopediaData: {
+          fish: fishContent
+        },  
         getResponseInformation: (language: LanguageEnum) => {
         // Implement your logic to get response information based on the language
       },
